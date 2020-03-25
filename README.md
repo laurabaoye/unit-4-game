07d0e8885094cf78bd0eaa342c3b2f8d40010e82
laurabaoye <laurabaoye@gmail.com>
2020-03-24 22:09:19 -0700

    edit

--- README.md
+++
@@ -1,4 +0,0 @@
-# Crystal Collector Game using javascript / jQuery
-
-This is crystal collector gaming design in html and css, DOM manipulation is done using jQuery and logic for the is in javascript/jQuery. <br>
-https://adnanniaz77.github.io/unit-4-game/
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,12 @@
 $('document').ready(function () {
-
-  // generate random number between 19 and 120 and assign it to a variable
   var randomNumber = Math.floor(Math.random() * 101 + 19);
-  // show the random number on screen
   $('#random_number').text(randomNumber);
 
-  // generate random number for each crystal and assign it to a variable
   var rubyRandomNumber = Math.floor(Math.random() * 11 + 1);
   var diamonRandomNumber = Math.floor(Math.random() * 11 + 1);
   var amberRandomNumber = Math.floor(Math.random() * 11 + 1)
   var jadeRandomNumber = Math.floor(Math.random() * 11 + 1);
 
-  // initial counter values
   var totalScore = 0;
   $('#total_score').text(totalScore);
 
@@ -21,8 +16,6 @@ $('document').ready(function () {
   var losses = 0;
   $('#losses').text(losses);
 
-  // ====================== functions ======================
-  // reset function 
   function reset() {
     randomNumber = Math.floor(Math.random() * 101 + 19);
     $('#random_number').text(randomNumber);
@@ -34,22 +27,19 @@ $('document').ready(function () {
     $('#total_score').text(totalScore);
   }
 
-  // winner function
   function winner() {
     wins++;
     $('#wins').text(wins);
     reset();
   }
 
-  // loser function
   function loser() {
     losses++;
     $('#losses').text(losses);
     reset();
   }
 
-  //===========================================================
-  // click function for ruby crystal
+
   $('.ruby').on('click', function () {
     totalScore += rubyRandomNumber;
     $('#total_score').text(totalScore);
@@ -61,7 +51,7 @@ $('document').ready(function () {
     }
   });
 
-  // click function for diamond crystal
+
   $('.diamond').on('click', function () {
     totalScore += diamonRandomNumber;
     $('#total_score').text(totalScore);
@@ -73,7 +63,6 @@ $('document').ready(function () {
     }
   });
 
-  // click function for amber crystal
   $('.amber').click(function () {
     totalScore += amberRandomNumber;
     $('#total_score').text(totalScore);
@@ -85,7 +74,6 @@ $('document').ready(function () {
     }
   });
 
-  // click function for jade crystal
   $('.jade').on('click', function () {
     totalScore += jadeRandomNumber;
     $('#total_score').text(totalScore);
